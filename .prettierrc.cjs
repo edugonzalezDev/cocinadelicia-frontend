c50e2cbb5835e014c2f1f4baf4c2cef865ff7872
Eduardo <elduz.g@gmail.com>
2025-10-20 23:43:38 -0300

    chore: agregar configuración de Prettier y archivos de ignorado

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+// .prettierrc.cjs
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,               // ; al final de línea
+  singleQuote: false,       // comillas dobles
+  printWidth: 100,          // buen balance para JSX
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "all",     // diffs más limpios en listas/params
+  bracketSpacing: true,     // { foo: bar }
+  bracketSameLine: false,   // en JSX el ">" va en línea nueva
+  arrowParens: "always",
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  htmlWhitespaceSensitivity: "css",
+
+  // Plugins
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.js",
+};
